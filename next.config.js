ca4448f8a42e6597efe1a06f830ffc61665bca4f
Sam Zamor <168610299+Splaticusamz@users.noreply.github.com>
2025-01-06 15:23:03 -0800

    fixed errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/generate-progress/stream',
+        destination: '/api/generate-progress/stream'
+      }
+    ]
+  },
   async headers() {
     return [
       {
